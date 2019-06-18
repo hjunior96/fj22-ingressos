c7239bb46878c584684c70eea24ea13620cf4d07
HamiltonJunior <hamilton.junior96@yahoo.com.br>
2019-06-18 11:44:23 -0300

    Funcionalidade de Salvar uma nova sessão no banco de dados

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,13 +2,35 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Sessao {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	
+	@ManyToOne
 	private Sala sala;
+	
+	@ManyToOne
 	private Filme filme;
 	
+	
+	@Deprecated
+	/**
+	 * @deprecated Hibernate Only
+	 */
+	public Sessao() {
+		super();
+	}
+
 	public Sessao(LocalTime horario, Sala sala, Filme filme) {
 		this.horario = horario;
 		this.sala = sala;
