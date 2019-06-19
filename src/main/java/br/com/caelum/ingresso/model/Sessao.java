6f0af9135ccbe9124a5d5cb4975bd54a491e5ccd
HamiltonJunior <hamilton.junior96@yahoo.com.br>
2019-06-19 10:18:53 -0300

    Preco em Sala, Filme e Sessão + Teste de Sessão

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -22,8 +24,9 @@ public class Sessao {
 	@ManyToOne
 	private Filme filme;
 	
+	private BigDecimal preco;
 	
-	@Deprecated
+	
 	/**
 	 * @deprecated Hibernate Only
 	 */
@@ -35,6 +38,7 @@ public class Sessao {
 		this.horario = horario;
 		this.sala = sala;
 		this.filme = filme;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 	
 	public Integer getId() {
@@ -61,6 +65,13 @@ public class Sessao {
 	public void setFilme(Filme filme) {
 		this.filme = filme;
 	}
-	
+
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 	
 }
