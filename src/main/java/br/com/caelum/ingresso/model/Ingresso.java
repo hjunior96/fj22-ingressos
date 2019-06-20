bb6b580372867495f15f639a1c6fe6eea4cd7de8
HamiltonJunior <hamilton.junior96@yahoo.com.br>
2019-06-20 10:38:31 -0300

    Tipos de ingresso e teste unitário para seleção de lugares

--- src/main/java/br/com/caelum/ingresso/model/Ingresso.java
+++ src/main/java/br/com/caelum/ingresso/model/Ingresso.java
@@ -3,19 +3,51 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Ingresso {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@ManyToOne
 	private Sessao sessao;
+	
+	
 	private BigDecimal preco;
+	
+	@ManyToOne
 	private Lugar lugar;
 	
+	@Enumerated(EnumType.STRING)
+	private TipoDeIngresso tipoDeIngresso;
+	
+	/**
+	 * @deprecated Hibernate only
+	 */
+	public Ingresso() {}
+	
 	public Ingresso(Sessao sessao, TipoDeIngresso tipo, Lugar lugar) {
-		this.sessao 	= sessao;
-		this.preco 		= tipo.applicaDesconto(sessao.getPreco());
-		this.lugar		= lugar;
+		this.sessao 		= sessao;
+		this.tipoDeIngresso = tipo; 
+		this.preco 			= tipo.applicaDesconto(sessao.getPreco());
+		this.lugar			= lugar;
 	}
 	
-	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
 	public Sessao getSessao() {
 		return sessao;
 	}
@@ -34,5 +66,11 @@ public class Ingresso {
 	}
 	public void setLugar(Lugar lugar) {
 		this.lugar = lugar;
+	}
+	public TipoDeIngresso getTipoDeIngresso() {
+		return tipoDeIngresso;
+	}
+	public void setTipoDeIngresso(TipoDeIngresso tipoDeIngresso) {
+		this.tipoDeIngresso = tipoDeIngresso;
 	}
 }
